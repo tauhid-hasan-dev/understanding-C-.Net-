356ed0e811b209f6f074d5f1e05141f294136c12
tauhid <altoryazilim23@gmail.com>
2023-12-18 11:16:06 +0300

    Add project files.

---
+++ cSharpTutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cSharpTutorial
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            int calculation = Add(Add(5, 6), Add(5, 7));
+            int result = Add(45, 67);
+            // Console.WriteLine(result);
+            // Console.WriteLine(calculation);
+            // Console.Read();
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+
+    }
+
+
+
+}
+
+
